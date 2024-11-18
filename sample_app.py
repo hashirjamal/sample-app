ca6bdaea32dee840d82c772b01c83de505d82da8
hashirjamal <hashirjamal18@gmail.com>
2024-11-18 19:08:10 +0500

    Changing script 2

--- sample_app.py
+++ sample_app.py
@@ -3,7 +3,7 @@ from flask import request
 sample = Flask(__name__)
 @sample.route("/")
 def main():
-	return "Hello this is Hashir's Lab 10 for SCD, called from " + request.remote_addr + "\n"
+	return "Hashir Lab " + request.remote_addr + "\n"
 
 if __name__ == "__main__":
 	sample.run(host="0.0.0.0", port=5050)
